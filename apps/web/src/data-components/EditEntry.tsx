08255f5c8fc4b35d81153b4444486b6c8d18f004
basti.paintner <basti.paintner@digitalservice.bund.de>
2025-12-07 21:27:24 +0100

    Wip

---
+++ apps/web/src/data-components/EditEntry.tsx
@@ -0,0 +1,31 @@
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useTRPC } from "../utils/trpc";
+import { Suspense, useContext } from "react";
+import { SelectedElementContext } from "../providers/SelectedElementProvider";
+
+function DisplayEntryList() {
+  const trpc = useTRPC();
+  const { entryId } = useContext(SelectedElementContext);
+
+  const { data } = useSuspenseQuery(trpc.entry.getById.queryOptions(entryId));
+
+  return (
+    <ul>
+      <li>{data.id}</li>
+      <li>{data.name}</li>
+      <li>{data.email}</li>
+      <li>{data.password}</li>
+    </ul>
+  );
+}
+
+export default function EditEntry() {
+  return (
+    <section>
+      <p>edit</p>
+      <Suspense fallback={<p>No Data</p>}>
+        <DisplayEntryList />
+      </Suspense>
+    </section>
+  );
+}
--- apps/web/src/pages/Index.tsx
+++ apps/web/src/pages/Index.tsx
@@ -2,10 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import { useTRPC } from "../utils/trpc";
 import Entries from "../data-components/Entries";
 import DisplayEntry from "../data-components/DisplayEntry";
+import { useState } from "react";
+import EditEntry from "../data-components/EditEntry";
 
 export default function Index() {
   const trpc = useTRPC();
   const usersQuery = useQuery(trpc.user.getAllUsers.queryOptions());
+  const [edit, setEdit] = useState(false);
 
   return (
     <div className="grid min-h-screen grid-flow-col grid-rows-[auto_1fr_auto] gap-y-4">
@@ -19,7 +22,10 @@ export default function Index() {
           <Entries />
         </section>
         <section>
-          <DisplayEntry />
+          <>
+            <button onClick={() => setEdit(!edit)}>Toggle edit</button>
+            {edit ? <EditEntry /> : <DisplayEntry />}
+          </>
         </section>
       </main>
       <footer className="bg-amber-50 p-4"> Footer</footer>
