bbd383f43df473cfbec70ba677c1fd7dbd11b25b
litc0de <litc0de@posteo.org>
2026-03-18 09:01:15 +0100

    chore: disable server logs for now

--- apps/server/src/logger.ts
+++ apps/server/src/logger.ts
@@ -1,13 +1,13 @@
 import { publicProcedure } from "./trpc";
 
 export const loggedProcedure = publicProcedure.use(async (opts) => {
-  const start = Date.now();
+  // const start = Date.now();
   const result = await opts.next();
-  const durationMs = Date.now() - start;
-  const meta = { path: opts.path, type: opts.type, durationMs };
+  // const durationMs = Date.now() - start;
+  // const meta = { path: opts.path, type: opts.type, durationMs };
 
-  if (result.ok) console.log("OK request timing:", meta);
-  else console.error("Non-OK request timing", meta);
+  // if (result.ok) console.log("OK request timing:", meta);
+  // else console.error("Non-OK request timing", meta);
 
   return result;
 });
