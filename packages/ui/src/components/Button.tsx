79e86d3c42bdb8cc7417fb5622e8ea7a2f823d69
litc0de <litc0de@posteo.org>
2026-09-01 10:46:40 +0200

    style(button): fix dropdown button link presentation

--- packages/ui/src/components/Button.tsx
+++ packages/ui/src/components/Button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
           "text-destructive hover:bg-destructive/10 hover:text-destructive aria-expanded:bg-destructive/30 aria-expanded:text-destructive",
         destructive:
           "bg-destructive/20 text-destructive hover:bg-destructive/30 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/70 dark:text-foreground dark:focus-visible:ring-destructive/40 dark:hover:bg-destructive/90",
-        link: "text-primary underline-offset-4 hover:underline",
+        link: "justify-start text-primary underline-offset-4 hover:underline",
       },
       size: {
         default:
